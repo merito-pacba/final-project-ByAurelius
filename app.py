1c7deca085bf512d8f9ec8f4638af178a6088b69
ByAurelius <ardaakgl07@gmail.com>
2026-02-16 16:01:29 +0000

    Refactored UI: Translated all content and templates to English

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Gezgin Günlüğü Projesine Hoş Geldiniz!"
+    return "welcome to traveller journal project!"
 
 if __name__ == '__main__':
     app.run(debug=True)
